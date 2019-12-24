f56aeafec0e30952a90436cb643d8cb6dcd2d3d2
Vee Satayamas <vsatayamas@gmail.com>
2019-12-24 23:21:49 +0700

    Use HoneySQL helpers and add a conditions

--- src/db1/core.clj
+++ src/db1/core.clj
@@ -1,7 +1,8 @@
 (ns db1.core
   (:require [hikari-cp.core :refer [make-datasource]]
             [clojure.java.jdbc :as j]
-            [honeysql.core :as sql])
+            [honeysql.core :as sql]
+            [honeysql.helpers :refer :all :as helpers])
   (:gen-class))
 
 (def datasource-options {:pool-name          "db-pool"
@@ -16,8 +17,10 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (j/with-db-connection [conn {:datasource @datasource}]
-    (println (j/query conn
-                      (-> {:select :* :from :person}
-                          sql/build
-                          sql/format)))))
+  (let [my-telno "038"]
+    (j/with-db-connection [conn {:datasource @datasource}]
+      (println (j/query conn
+                        (-> (select :*)
+                            (from :person)
+                            (where [:= :telno my-telno])
+                            sql/format))))))
